a35148563904cea62347af23ee53150bd8d77cd5
unknown <douglassalvagni@gmail.com>
2020-12-30 14:32:12 +0000

    start project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import Nav from '../components/nav'
+import { NextPage } from 'next';
+import Nav from '../components/nav';
 
-export default function IndexPage() {
+const IndexPage: NextPage = () => {
   return (
     <div>
       <Nav />
-      <div className="py-20">
+      <div className="py-10">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default IndexPage;
